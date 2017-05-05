453c50b2a89c38d7209bc15edfd62f1a1863ee78
alex <sklif.85@gmail.com>
2017-05-05 13:15:18 +0300

    solution_1/1

--- solution.c
+++ solution.c
@@ -3,6 +3,19 @@
 
 void place_token(t_filler **filler)
 {
+	int i;
+	int j;
 
+	i = -1;
+	while (++i <= (*filler)->plato->x)
+	{
+		j = -1;
+		while (++j <= (*filler)->token->n)
+		{
+
+
+
+		}
+	}
 
 }
